5b7f5a692ad8301fd44808195c764f65297f8c84
dmsehuang <dmsehuang@yahoo.com>
2014-07-30 23:15:36 -0700

    Point.java modified

---
+++ Point.java
@@ -0,0 +1,92 @@
+/*************************************************************************
+ * Name: Huijing Huang
+ * Email: dmsehuang@yahoo.com
+ *
+ * Compilation:  javac Point.java
+ * Execution: java Point
+ * Dependencies: StdDraw.java
+ *
+ * Description: An immutable data type for points in the plane.
+ *
+ *************************************************************************/
+
+import java.util.Comparator;
+
+public class Point implements Comparable<Point> {
+
+    // compare points by slope
+    public final Comparator<Point> SLOPE_ORDER = new SlopeOrder();       // YOUR DEFINITION HERE
+
+    private final int x;                              // x coordinate
+    private final int y;                              // y coordinate
+
+    // create the point (x, y)
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+    
+    private class SlopeOrder implements Comparator<Point> {
+        public int compare (Point p1, Point p2) {
+            double s1 = slopeTo(p1);
+            double s2 = slopeTo(p2);
+            if (s1 == s2) {
+                return 0;
+            }else if (s1 < s2) {
+                return -1;
+            }else {
+                return 1;
+            }
+        }
+    }
+
+    // plot this point to standard drawing
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    // draw line between this point and that point to standard drawing
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    // slope between this point and that point
+    public double slopeTo(Point that) {
+        /* YOUR CODE HERE */
+        if (this.x == that.x) {
+            if (this.y == that.y) return Double.NEGATIVE_INFINITY; // degenerate line segment
+            return Double.POSITIVE_INFINITY; // vertical line segment
+        }
+        if (this.y == that.y) return +0.0; // horizontal line segment has positive zero slope
+        
+        // normal case
+        double diff_y = (double)(this.y - that.y);
+        double diff_x = (double)(this.x - that.x);
+        return diff_y/diff_x;
+    }
+
+    // is this point lexicographically smaller than that one?
+    // comparing y-coordinates and breaking ties by x-coordinates
+    public int compareTo(Point that) {
+        /* YOUR CODE HERE */
+        if (this.y == that.y) {
+            return this.x - that.x;
+        }else {
+            return this.y - that.y;
+        }
+    }
+
+    // return string representation of this point
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    // unit test
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
+    }
+}
